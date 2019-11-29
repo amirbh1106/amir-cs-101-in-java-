333d9ab6ad8e071b9b7261df755ebc4c54afebe6
amirbh1106 <42180486+amirbh1106@users.noreply.github.com>
2019-11-29 22:58:11 +0200

    updated
    error catching

--- cs 101 projects/Triangle.java
+++ cs 101 projects/Triangle.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
 public class Triangle {
         public static void main(String[] args) {
-            Scanner reader = new Scanner(System.in);
+            Scanner Reader = new Scanner(System.in);
 
             System.out.println("this program calculates the area and the perimeter of a given triangle"); // ui
-            System.out.println(" please enter the three lengths of the tringel sides"); // ui
-            int firstinp = reader.nextInt();    //  inputs
-            int secinp = reader.nextInt();
-            int lestinp = reader.nextInt();
+            System.out.println("please enter the three lengths of the tringel sides"); // ui
+            int firstinp = 0;   //  inputs
+            int secinp = 0;
+            int lestinp = 0;
+
+            try{
+                firstinp = Reader.nextInt();
+                secinp = Reader.nextInt();
+                lestinp = Reader.nextInt();
+            } catch(Exception e){
+                System.out.println("int please");
+                System.exit(1);
+            }
+            
+                    
             double s = (firstinp + secinp + lestinp) /2.0; // calc s and put it in double
-            double result;
+            
 
 
             if (firstinp > 0 && secinp > 0 && lestinp > 0){
-                result = s * (s - firstinp ) * (s - secinp ) * (s - lestinp);
+                double result = s * (s - firstinp ) * (s - secinp ) * (s - lestinp);
                         if(Double.isNaN(result)) {
                             System.out.println("sorry but i cant right now ");
                         }else{
